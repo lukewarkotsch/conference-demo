7b8faf1068814dbb839112db8b2ff21fee9d8415
Luke Warkotsch <luke.warkotsch@telus.com>
2020-09-08 13:39:24 -0700

    war packaging

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PersistenceConfiguration {
-/*    @Bean
+    @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url( "jdbc:postgresql://localhost:5432/conference-demo" );
@@ -16,5 +16,5 @@ public class PersistenceConfiguration {
         builder.password( "RlIuKkEeR!16" );
         System.out.println( "Custom DataSource Bean has been initialized and set." );
         return builder.build();
-    }*/
+    }
 }
